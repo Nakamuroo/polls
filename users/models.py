4cb5889cd2ea1aa7ee30773b53c133cbe0294e64
Станислав Гладких <gr404_glsan@lab-404-1.tomtit-tomsk.ru>
2022-11-30 16:12:32 +0700

    add life time

--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-import datetime
-from django.utils import timezone
 
 
 # Extending User Model Using a One-To-One Link
@@ -30,9 +30,11 @@ class Profile(models.Model):
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
+    life_time = models.DateTimeField(default=datetime.now() + timedelta(days=1),
+                                     verbose_name='Время жизни',
+                                     null=False)
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= datetime.now() - timedelta(days=1)
 
     def __str__(self):
         return self.question_text
